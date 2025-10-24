1318eef35a410c70094ab1f8ad2d72e9981e01e0
Mario <mario.vieilledent@gmail.com>
2025-10-24 11:40:47 +0200

    wsfdhlie

--- src/components/recipes/NotFoundRecipe.tsx
+++ src/components/recipes/NotFoundRecipe.tsx
@@ -1,19 +1,19 @@
 import { RECIPES_PATH } from "../../utils/routes";
 import { useTranslation } from "../../utils/TranslationContext";
 import NavigateTo from "../NavigateTo";
+import notFoundImage from "/404.png";
 
 const NotFoundRecipe = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 gap-8 bg-gray-100">
+      <h1 className="text-4xl font-bold text-center">{t("pageNotFound")}</h1>
+
       <NavigateTo location={`${RECIPES_PATH}`} />
-      <h1 className="text-4xl font-bold text-center mt-20">
-        {t("pageNotFound")}
-      </h1>
 
       <img
-        src="/public/404.png"
+        src={notFoundImage}
         alt="404 Not Found"
         className="w-1/2 max-w-sm mt-10"
       />
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,18 +1,18 @@
 import Home from "../components/Home";
 import { useTranslation } from "../utils/TranslationContext";
+import notFoundImage from "/404.png";
 
 const NotFound = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 gap-8 bg-gray-100">
+      <h1 className="text-4xl font-bold text-center">{t("pageNotFound")}</h1>
+
       <Home />
-      <h1 className="text-4xl font-bold text-center mt-20">
-        {t("pageNotFound")}
-      </h1>
 
       <img
-        src="/public/404.png"
+        src={notFoundImage}
         alt="404 Not Found"
         className="w-1/2 max-w-sm mt-10"
       />
