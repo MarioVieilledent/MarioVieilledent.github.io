040cd9880572440dc16777ed12a8f3f1b010d549
Mario Vieilledent <mario.vieilledent@gmail.com>
2026-02-25 14:52:24 +0100

    Big dep update

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,4 @@
 import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
@@ -11,19 +9,16 @@ export default tseslint.config(
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
     },
     plugins: {
-      "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
       "@typescript-eslint/no-explicit-any": "off",
     },
-  }
+  },
 );
