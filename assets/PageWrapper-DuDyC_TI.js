23ca9e1ca238d664399636841a55bac2b438fd27
Mario Vieilledent <mario.vieilledent@gmail.com>
2026-09-08 15:22:46 +0200

    Updates

---
+++ assets/PageWrapper-DuDyC_TI.js
@@ -0,0 +1,1 @@
+import{c as e,u as t}from"./index-D8c3OX5l.js";var n=t(),r=({children:t})=>{let r=e();return(0,n.jsx)(`div`,{className:r?`flex flex-col`:`flex flex-col max-w-6xl mx-auto gap-8 px-8`,children:t})};export{r as t};
