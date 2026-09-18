77270495c1fdb4624530dce98d244aedd68035a2
Mario Vieilledent <mario.vieilledent@gmail.com>
2026-09-18 09:54:26 +0200

    Updates

---
+++ assets/PageWrapper-Dr7eMoiE.js
@@ -0,0 +1,1 @@
+import{v as e}from"./index-B62yA3dl.js";import{t}from"./isMobileHook-LTx7otIu.js";var n=e(),r=({children:e})=>{let r=t();return(0,n.jsx)(`div`,{className:r?`flex flex-col`:`flex flex-col max-w-6xl mx-auto gap-8 px-8`,children:e})};export{r as t};
